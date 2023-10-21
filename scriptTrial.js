54efe88ea4ef7acb9543a2ca51a541a040fd7a8d
valemisem <valentina.misem@gmail.com>
2023-10-21 21:03:31 +0200

    edited

---
+++ scriptTrial.js
@@ -0,0 +1,10 @@
+let ageUser
+ageUser = "26" // строка
+console.log(ageUser)
+
+console.log(typeof(String(ageUser)));
+
+let num = Number(ageUser); // становится числом 26
+console.log(ageUser)
+
+console.log(typeof(Number(ageUser)))
