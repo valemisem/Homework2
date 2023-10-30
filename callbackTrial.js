ce8eba16fa4fe21e08a18d2b9c00f290b2fde091
valemisem <valentina.misem@gmail.com>
2023-10-30 23:34:14 +0100

    j

--- callbackTrial.js
+++ callbackTrial.js
@@ -11,3 +11,27 @@ function calculateSavings (expenses = calculateExpenses(20, 40, 500, 500), salar
 // console.log(calculateSavings(calculateExpenses(20, 40, 500, 500), 10000))
 
 console.log(calculateSavings(10, 400))
+
+
+// signature
+function doSmth (Ar1, Ar2){ // параметром функции может быть ФУНКЦИЯ 
+    let x = Ar1 + Ar2()
+    return x
+}
+
+let result = doSmth(3, function(){
+    return 6+10
+} )
+
+console.log(result)
+
+// comparisson
+x = 5
+y = 7
+z = '5'
+console.log(x == y) // != - НЕравно
+console.log(x == z)
+console.log(x === z) // строгое равенство
+
+
+
